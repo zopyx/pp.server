2ae2f437dec142221cd0f1fc65ce133d4ba52e13
zopyx <yet@gmx.de>
2013-06-22 15:47:32 +0200

    logging

--- src/pp.client/pp/client/unoconv.py
+++ src/pp.client/pp/client/unoconv.py
@@ -3,18 +3,25 @@
 import os
 import plac
 import xmlrpclib
+
 @plac.annotations(
     input_filename=('Source file to be converted', 'positional'),
     format=('Output format (default=pdf)', 'option', 'f'),
     output=('Write converted file to custom filename', 'option', 'o'),
     server_url=('URL of Produce & Publish XMLRPC API)', 'option', 's'),
     async=('Perform conversion asynchronously)', 'flag', 'a'),
+    verbose=('Verbose mode)', 'flag', 'v'),
 )
-def main_(input_filename, 
-          format='pdf', 
-          output='',
-          async=False, 
-          server_url='http://localhost:6543/api'):
+def unoconv(input_filename, 
+           format='pdf', 
+           output='',
+           async=False, 
+           server_url='http://localhost:6543/api',
+           verbose=False):
+
+    def log(s):
+        if verbose:
+            print s
 
     server = xmlrpclib.ServerProxy(server_url)
     result = server.unoconv(os.path.basename(input_filename),
@@ -22,7 +29,7 @@ def main_(input_filename,
                             format,
                             async)
     if async:
-        print result
+        log(result)
     else:
         if result['status'] == 'OK':
             if not output:
@@ -30,14 +37,16 @@ def main_(input_filename,
                 output= base + '.' + format
             with open(output, 'wb') as fp:
                 fp.write(result['data'].data)
-            print 'Output filename: {}'.format(output)
+            log('Output filename: {}'.format(output))
         else:
-            print 'An error occured'
-            print 'Output:'
-            print result['output']
-    
+            log('An error occured')
+            log('Output:')
+            log(result['output'])
+
+    return result
+
 def main():
-    plac.call(main_)
+    plac.call(unoconv)
 
 if __name__ == '__main__':
     main()
