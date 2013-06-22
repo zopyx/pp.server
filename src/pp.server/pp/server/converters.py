e1f8bae7aa54910158057f3a4a16d74b416d47a3
zopyx <yet@gmx.de>
2013-06-22 15:41:49 +0200

    updated

--- src/pp.server/pp/server/converters.py
+++ src/pp.server/pp/server/converters.py
@@ -1,4 +1,6 @@
 import os
+import zlib
+import zipfile
 from pp.server import util
 
 def unoconv(input_filename, output_format):
@@ -10,7 +12,35 @@ def unoconv(input_filename, output_format):
     dest_filename = base + '.' + output_format
     cmd = 'unoconv -f "{}" "{}"'.format(output_format, input_filename)
     status, output = util.runcmd(cmd)
-
     return dict(status=status,
                 output=output,
                 filename=dest_filename)
+
+
+def pdf(work_dir, work_file, converter):
+    """ Converter a given ZIP file
+        containing input files (HTML + XML) and asset files
+        to PDF.
+    """
+
+    # unzip archive first
+    zf = zipfile.ZipFile(work_file)
+    for name in zf.namelist():
+        filename = os.path.join(work_dir, name)
+        with open(filename, 'wb') as fp:
+            fp.write(zf.read(name))
+
+    source_html = os.path.join(work_dir, 'index.html')
+    target_pdf = os.path.join(work_dir, 'out.pdf')
+
+    if converter == 'princexml':
+        cmd = 'prince -v "{}" "{}"'.format(source_html, target_pdf) 
+    elif converter == 'pdfreactor':
+        cmd = 'pdfreactor -v debug "{}" "{}"'.format(source_html, target_pdf) 
+    else:
+        raise NotImplementedError('No support for converter "{}"'.format(converter))
+
+    status, output = util. runcmd(cmd)
+    return dict(status=status,
+                output=output,
+                filename=target_pdf)
--- src/pp.server/pp/server/views.py
+++ src/pp.server/pp/server/views.py
@@ -3,6 +3,7 @@ import os
 import time
 import uuid
 import xmlrpclib
+import zlib
 from pyramid_xmlrpc import XMLRPCView
 from webob import Response
 from datetime import datetime
@@ -59,6 +60,44 @@ class XMLRPC(XMLRPCView):
             if result['status'] == 0: #OK
                 return dict(status='OK',
                             data=xmlrpclib.Binary(open(result['filename'], 'rb').read()),
+                            output=result['output'])
+            else: # error
+                return dict(status='ERROR',
+                            output=result['output'])
+
+    def pdf(self,
+            zip_data, 
+            converter='princexml',
+            async=False):
+
+        new_id = str(uuid.uuid4())
+        work_dir = os.path.join(queue_dir, new_id)
+        os.mkdir(work_dir)
+        work_file = os.path.join(work_dir, 'in.zip')
+        with open(work_file, 'wb') as fp:
+            fp.write(zip_data.data)
+
+        if async:
+            raise NotImplementedError
+            tasks.unoconv.delay(job_id=new_id,
+                                work_dir=work_dir,
+                                input_filename=work_file,
+                                output_format=output_format)
+            return dict(id=new_id, message=u'Conversion request queued')
+        else:
+            ts = time.time()
+            LOG.info('START: pdf({}, {}, {}, {})'.format(new_id, work_file, converter, async))
+            result = converters.pdf(work_dir, work_file, converter)
+            duration = time.time() - ts
+            LOG.info('END : pdf({} {} sec): {}'.format(new_id, duration, result['status']))
+            if result['output']:
+                LOG.info('OUTPUT: pdf({}):\n{}'.format(new_id, result['output']))
+            if result['status'] == 0: #OK
+                pdf_data = open(result['filename'], 'rb').read()
+                pdf_data = zlib.compress(pdf_data)
+                return dict(status='OK',
+                            data=xmlrpclib.Binary(pdf_data),
+                            compression='zlib',
                             output=result['output'])
             else: # error
                 return dict(status='ERROR',
