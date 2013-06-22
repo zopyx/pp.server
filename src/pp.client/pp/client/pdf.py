cb9c6cb383dcfded47840a985d1d4526cdc1cfd7
zopyx <yet@gmx.de>
2013-06-22 15:48:08 +0200

    added

---
+++ src/pp.client/pp/client/pdf.py
@@ -0,0 +1,77 @@
+""" XMLRPC client to access the unoconv API of the Produce & Publish server """
+
+import os
+import plac
+import xmlrpclib
+import zipfile
+import zlib
+import tempfile
+
+def makeZipFromDirectory(directory):
+    """ Generate a ZIP file from a directory containing all its
+        contents. Returns the filename of the generated ZIP file.
+    """
+
+    directory = os.path.abspath(directory)
+    zip_filename = tempfile.mktemp()
+    ZF = zipfile.ZipFile(zip_filename, 'w')
+    for dirname, dirnames, filenames in os.walk(directory):
+        for fname in filenames:
+            arcname = os.path.join(dirname, fname).replace(directory + os.path.sep, '')
+            fullname = os.path.abspath(os.path.join(dirname, fname))
+            ZF.write(fullname, arcname)
+    ZF.close()
+    return zip_filename
+
+@plac.annotations(
+    source_directory=('Source directory containing content and assets to be converted', 'positional'),
+    converter=('PDF converter to be used (princexml, pdfreactor)', 'option', 'c'),
+    output=('Write result ZIP to given .zip filename', 'option', 'o'),
+    server_url=('URL of Produce & Publish XMLRPC API)', 'option', 's'),
+    async=('Perform conversion asynchronously)', 'flag', 'a'),
+    verbose=('Verbose mode', 'flag', 'v'),
+)
+def main_(source_directory,
+          converter='princexml', 
+          output='',
+          async=False, 
+          server_url='http://localhost:6543/api',
+          verbose=False):
+
+    def log(s):
+        if verbose:
+            print s
+
+    zip_filename = makeZipFromDirectory(source_directory)
+    server = xmlrpclib.ServerProxy(server_url)
+    result = server.pdf(xmlrpclib.Binary(open(zip_filename, 'rb').read()),
+                        converter,
+                        async)
+
+    os.unlink(zip_filename)
+
+    if async:
+        log(result)
+    else:
+        if result['status'] == 'OK':
+            if not output:
+                base, ext = os.path.splitext(zip_filename)
+                output= base + '.pdf'
+            with open(output, 'wb') as fp:
+                if result['compression'] == 'zlib':
+                    fp.write(zlib.decompress(result['data'].data))
+                else:
+                    fp.write(result['data'].data)
+            log('Output filename: {}'.format(output)
+        else:
+            log('An error occured')
+            log('Output:')
+            log(result['output'])
+
+    return result
+    
+def main():
+    plac.call(main_)
+
+if __name__ == '__main__':
+    main()
