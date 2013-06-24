d0a1d36e3ce27d7686d0f4173a5d8e8b29c04762
zopyx <yet@gmx.de>
2013-06-24 19:22:26 +0200

    updated

--- src/pp.server/setup.py
+++ src/pp.server/setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(name='pp.server',
       version='0.1',
-      description='pp.server',
+      description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
         "Programming Language :: Python",
