a0a41b7489a7d54a7824e4c070445b7fab96ab9a
zopyx <yet@gmx.de>
2013-06-28 12:58:34 +0200

    more tests

--- src/pp.server/pp/server/tests/test_utils.py
+++ src/pp.server/pp/server/tests/test_utils.py
@@ -1,7 +1,23 @@
+################################################################
+# pp.server - Produce & Publish Server
+# (C) 2013, ZOPYX Ltd, Tuebingen, Germany
+################################################################
 
+import os
 from pp.server import util
 
 
 def test_which():
     assert util.which('vim') 
     assert not util.which('unknown')
+
+def test_checkEnvironmentNonExisting():
+    assert not util.checkEnvironment('FOO') 
+
+def test_checkEnvironmentNonExistingDirectory():
+    os.environ['FOO'] = '/foo'
+    assert not util.checkEnvironment('FOO') 
+
+def test_checkEnvironmentExistingDirectory():
+    os.environ['FOO'] = os.getcwd()
+    assert util.checkEnvironment('FOO') 
