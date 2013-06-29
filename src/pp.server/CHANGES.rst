4f89307c833ea4db6a3a6c4f8a170d87925f7454
zopyx <yet@gmx.de>
2013-06-29 07:58:46 +0200

    Prepare pp.server 0.1.7.

--- src/pp.server/CHANGES.rst
+++ src/pp.server/CHANGES.rst
@@ -1,3 +1,9 @@
+0.1.7 (29.06.2013)
+------------------
+- more tests
+- fixes
+- updated documentation
+
 0.1.5 (27.06.2013)
 ------------------
 - test for synchronous operations
--- src/pp.server/setup.py
+++ src/pp.server/setup.py
@@ -24,7 +24,7 @@ tests_require = [
 ]
 
 setup(name='pp.server',
-      version='0.1.6',
+      version='0.1.7',
       description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
