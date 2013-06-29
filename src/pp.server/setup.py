161d49356de9e08ed4f28e6f64748808b104619d
zopyx <yet@gmx.de>
2013-06-29 08:19:34 +0200

    updated

--- src/pp.server/setup.py
+++ src/pp.server/setup.py
@@ -24,7 +24,7 @@ tests_require = [
 ]
 
 setup(name='pp.server',
-      version='0.1.7',
+      version='0.1.8',
       description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
