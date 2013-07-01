1034d1761808531783ab95bd420f9cdcc4b304b7
Andreas Jung <yet@gmx.de>
2013-07-01 08:58:02 +0200

    updated

--- src/pp.client/pp/client/logger.py
+++
@@ -1,8 +0,0 @@
-################################################################
-# pp.client - Produce & Publish Python Client
-# (C) 2013, ZOPYX Ltd, Tuebingen, Germany
-################################################################
-
-import logging 
-
-LOG = logging.getLogger()
--- src/pp.server/pp/server/__init__.py
+++ src/pp.server/pp/server/__init__.py
@@ -16,3 +16,19 @@ def main(global_config, **settings):
     config.add_view(XMLRPC_API, name='api')
     config.scan()
     return config.make_wsgi_app()
+
+
+
+# Monkey patch pyramid_xmlrpc.parse_xmlrpc_request in order
+# to avoid (stupid) DOS request body size check.
+import xmlrpclib
+import pyramid_xmlrpc
+
+def parse_xmlrpc_request(request):
+    """ Deserialize the body of a request from an XML-RPC request
+    document into a set of params and return a two-tuple.  The first
+    element in the tuple is the method params as a sequence, the
+    second element in the tuple is the method name."""
+    params, method = xmlrpclib.loads(request.body)
+    return params, method
+pyramid_xmlrpc.parse_xmlrpc_request = parse_xmlrpc_request
