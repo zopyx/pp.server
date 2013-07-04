8b3014f3648340a05d06bcacc72b6ba2c7566eef
Andreas Jung <yet@gmx.de>
2013-07-04 20:11:41 +0200

    dependency cleanup

--- src/pp.server/setup.py
+++ src/pp.server/setup.py
@@ -8,8 +8,6 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = [
     'pyramid',
-    'pyramid_debugtoolbar',
-    'pyramid_xmlrpc',
     'waitress',
     'sqlalchemy',
     'celery',
