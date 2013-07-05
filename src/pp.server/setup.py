738190fb4aad6527b081796a0aa4f2d57718d153
Andreas Jung <yet@gmx.de>
2013-07-05 07:09:32 +0200

    updated paths

--- src/pp.server/setup.py
+++ src/pp.server/setup.py
@@ -3,8 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+README = open(os.path.join(here, 'doc', 'source', 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'doc', 'source', 'CHANGES.rst')).read()
 
 requires = [
     'pyramid',
