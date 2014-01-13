2ff8a1c2de5d58e979969b59fc0bb9d05de49848
zopyx <yet@gmx.de>
2014-01-13 12:58:13 +0100

    cleanup

--- src/pp.server/pp/server/authorization.py
+++ src/pp.server/pp/server/authorization.py
@@ -2,10 +2,8 @@
 
 from pyramid.httpexceptions import HTTPForbidden
 
-def beta_token(request):
-    beta_token = request.registry.settings.get('pp.beta_token')
+def token_auth(request):
+    beta_token = request.registry.settings.get('pp.authentication_token')
     token_request = request.headers.get('pp-token')
-    print token_request
-    print beta_token
     if token_request != beta_token:
-        raise HTTPForbidden
+        raise HTTPForbidden('Improper authorization token')
