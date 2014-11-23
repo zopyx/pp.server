7086ea89adfc0739843e4d869b2ecfce14a50df6
Andreas Jung <yet@gmx.de>
2014-11-23 16:49:36 +0100

    updated

--- src/pp.server/setup.py
+++ src/pp.server/setup.py
@@ -26,7 +26,7 @@ tests_require = [
 ]
 
 setup(name='pp.server',
-      version='0.5.2',
+      version='0.5.3',
       description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
