feebc52213346d2188904417d4c3ab0bbe65412e
Andreas Jung <yet@gmx.de>
2015-02-15 09:22:42 +0100

    updated

--- pp/server/__init__.py
+++ pp/server/__init__.py
@@ -3,8 +3,7 @@
 # (C) 2013, ZOPYX,  Tuebingen, Germany
 ################################################################
 
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
 
 from pyramid.config import Configurator
 
