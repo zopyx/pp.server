1030b1eb66435deed3ed7ba9c2d86752188b7b88
Mikel Larreategi <mlarreategi@codesyntax.com>
2015-04-20 11:30:00 +0200

    parameter cmd_options is required in converters.pdf

--- pp/server/tasks.py
+++ pp/server/tasks.py
@@ -3,8 +3,8 @@
 # (C) 2013, ZOPYX,  Tuebingen, Germany
 ################################################################
 
-""" 
-Celery tasks 
+"""
+Celery tasks
 """
 
 from celery import Celery, current_task
@@ -16,7 +16,7 @@ celery = Celery('tasks', broker='sqla+sqlite:///celerydb.sqlite')
 @celery.task
 def unoconv(job_id, work_dir, input_filename, output_format):
     """ asyncronous Unoconv processing """
-    current_task.update_state(state='PROGRESS', 
+    current_task.update_state(state='PROGRESS',
                               meta=dict(job_id=job_id, work_dir=work_dir))
     result = converters.unoconv(work_dir, input_filename, output_format)
     if result['status'] == 0:
@@ -28,9 +28,9 @@ def unoconv(job_id, work_dir, input_filename, output_format):
 @celery.task
 def pdf(job_id, work_dir, work_file, converter):
     """ asyncronous PDF processing """
-    current_task.update_state(state='PROGRESS', 
+    current_task.update_state(state='PROGRESS',
                               meta=dict(job_id=job_id, work_dir=work_dir))
-    result = converters.pdf(work_dir, work_file, converter)
+    result = converters.pdf(work_dir, work_file, converter, cmd_options='')
     if result['status'] == 0:
         current_task.update_state(state='OK', meta=result)
     else:
