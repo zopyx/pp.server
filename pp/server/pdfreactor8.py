73c8cfb44c03be75fd23e3e1145932bd32c230a8
zopyx <yet@gmx.de>
2015-11-30 17:45:17 +0100

    updated

--- pp/server/pdfreactor8.py
+++ pp/server/pdfreactor8.py
@@ -7,6 +7,8 @@ import subprocess
 argv = sys.argv[1:]
 
 pdfreactor_path = os.environ.get('PDFREACTOR_PATH', '/opt/PDFreactor8')
+if not os.path.exists(pdfreactor_path):
+    raise RuntimeError('No PDFreactor installation found at {}'.format(pdfreactor_path))
 bin_path = os.path.join(pdfreactor_path, 'bin', 'pdfreactor.py') 
 if not os.path.exists(bin_path):
     raise RuntimeError('No PDFreactor 8 script found at {}'.format(bin_path))
