9b5685368e0fd418962af40889b3775fde293ba4
Andreas Jung <andreas@andreas-jung.com>
2017-08-17 10:31:32 +0200

    updated

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 ]
 
 setup(name='pp.server',
-      version='1.0.7',
+      version='1.0.7.1',
       description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
