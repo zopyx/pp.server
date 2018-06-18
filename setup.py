84046b3f8a39a4de434cee564f88bb6c3997d9b0
Andreas JungÂ <info@zopyx.com>
2018-06-18 17:01:04 +0200

    updated

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ requires = [
     'waitress',
     'plac',
     'circus',
-    'pp.client-python'
+    'pp.client-python',
+    'tornado==4.2',
     ]
 
 tests_require = [
@@ -28,7 +29,7 @@ tests_require = [
 ]
 
 setup(name='pp.server',
-      version='1.0.8.3',
+      version='1.0.8.4',
       description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
