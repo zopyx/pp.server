8cf9c5ec121c75b8f1e4bd89c96f6deb81de9e00
Andreas Jung <info@zopyx.com>
2019-03-09 08:16:24 +0100

    minor configuration changes

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = [
 ]
 
 setup(name='pp.server',
-      version='2.0.5',
+      version='2.0.6',
       description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
