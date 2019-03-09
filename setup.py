c0f2845200f7a01083f0fe9d43ad5340f063c9b1
Andreas Jung <info@zopyx.com>
2019-03-09 16:13:03 +0100

    updated

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = [
 ]
 
 setup(name='pp.server',
-      version='2.0.6.1',
+      version='2.0.6.7',
       description='pp.server - Produce & Publish Server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
