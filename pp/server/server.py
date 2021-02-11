002d3bd68588ce3c67681942a22c28d8bce9d1ac
Andreas Jung <info@zopyx.com>
2021-02-11 10:50:40 +0100

    template added

--- pp/server/server.py
+++ pp/server/server.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import pkg_resources
 from typing import Optional
 
 from fastapi import FastAPI
@@ -10,18 +13,30 @@ from fastapi.templating import Jinja2Templates
 
 from pp.server import registry
 
-templates = Jinja2Templates(directory="templates")
-
 # bootstrap
 registry._register_converters()
 
 app = FastAPI()
 
+dirname = os.path.dirname(__file__)
+static_dir = os.path.join(dirname, 'static')
+templates_dir = os.path.join(dirname, 'templates')
+
+
+templates = Jinja2Templates(directory=templates_dir)
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+
+
+
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
+    version = pkg_resources.require("pp.server")[0].version
     params = {
         "request": request, 
-        "converters": registry.available_converters()
+        "converters": registry.available_converters(),
+        "version": version,
+        "python_version": sys.version,
     }
     return templates.TemplateResponse("index.html", params)
 
@@ -30,6 +45,7 @@ def index(request: Request):
 def converters():
     """ Return names of all converters """
     return dict(converters=registry.available_converters())
+
 
 @app.get("/converter")
 def has_converter(converter_name):
