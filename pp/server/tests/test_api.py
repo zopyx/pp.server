4fdd2b02266f8f721ec144175b2ecbba891d86c5
Andreas Jung <info@zopyx.com>
2021-02-12 14:38:11 +0100

    adopted tests

--- pp/server/tests/test_api.py
+++ pp/server/tests/test_api.py
@@ -10,32 +10,27 @@ import unittest
 import zipfile
 import tempfile
 import zipfile
-from pyramid import testing
 
+from fastapi.testclient import TestClient
 
-class Base(unittest.TestCase):
+from pp.server.server import app
 
-    def setUp(self):
-        from pp.server import main
-        from webtest import TestApp
-        app = main({})
-        self.config = testing.setUp()
-        self.testapp = TestApp(app)
+client = TestClient(app)
 
-class PDFTests(Base):
+class PDFTests(unittest.TestCase):
 
     def test_index(self):
-        result = self.testapp.get('/', status=200)
-        assert '<title>Produce &amp; Publish Server</title> ' in result
+        result = client.get('/')
+        assert result.status_code == 200
+        assert '2021' in result.text
 
-    def test_princexml(self):
-        self._convert_pdf('princexml')
+    def test_has_converter(self):
+        result = client.get('/converter?converter_name=prince')
+        assert result.status_code == 200
+        assert result.json() == dict(has_converter=True)
 
-    def test_pdfreactor(self):
-        self._convert_pdf('pdfreactor')
-
-    def test_unknown_converter(self):
-        self._convert_pdf('does.not.exist', expected='ERROR')
+    def test_prince(self):
+        self._convert_pdf("prince")
 
     def _convert_pdf(self, converter, expected='OK'):
 
@@ -49,59 +44,17 @@ class PDFTests(Base):
             zip_data = fp.read()
         os.unlink(zip_name)
 
-        params = dict(converter=converter)
+        params = dict(converter=converter, cmd_options=" ", data=base64.encodebytes(zip_data))
         upload_files = [('file', 'in.zip', zip_data)]
-        result = self.testapp.post('/api/1/pdf', params, upload_files=upload_files, status=200)
-        params = json.loads(result.body)
+        result = client.post('/convert', params)
+        params = result.json()
 
         if expected == 'OK':
             assert params['status'] == 'OK'
             assert 'output' in params
-            pdf_data = base64.decodestring(params['data'])
-            assert pdf_data.startswith('%PDF-1.4')
+            pdf_data = base64.decodebytes(params['data'].encode("ascii"))
+            assert pdf_data.startswith(b'%PDF-1.')
         else:
             assert params['status'] == 'ERROR'
             assert 'Unknown converter' in params['output']
 
-class Unoconvtests(Base):
-
-    def _unoconv(self, input_file, format, expected='OK'):
-
-        docx_fname = os.path.join(os.path.dirname(__file__), input_file)
-        with open(docx_fname, 'rb') as fp:
-            docx_data = fp.read()
-
-        params = dict(filename=input_file, output_format=format)
-        upload_files = [('file', input_file, docx_data)]
-        result = self.testapp.post('/api/1/unoconv', params, upload_files=upload_files, status=200)
-        params = json.loads(result.body)
-
-        if expected == 'OK':
-            assert params['status'] == 'OK'
-            assert 'output' in params
-            zip_name = tempfile.mktemp(suffix='.zip')
-            with open(zip_name, 'wb') as fp:
-                zip_data = base64.decodestring(params['data'])
-                fp.write(zip_data)
-
-            with zipfile.ZipFile(zip_name, 'r') as zf:
-                for name in zf.namelist():
-                    base, ext = os.path.splitext(name)
-                    if ext in ('.pdf', '.html'):
-                        data = zf.read(name)
-                        os.unlink(zip_name)
-                        return data
-        else:
-            assert params['status'] == 'ERROR'
-            assert 'is not known to unoconv' in params['output']
-    
-    def test_docx2pdf(self):
-        pdf_data = self._unoconv('test.docx', 'pdf')
-        assert pdf_data.startswith('%PDF-1.4')
-
-    def test_docx2html(self):
-        html_data = self._unoconv('test.docx', 'html')
-        assert html_data.startswith('<!DOCTYPE')
-
-    def test_docx2unknown(self):
-        self._unoconv('test.docx', 'unknown', expected='ERROR')
--- pp/server/tests/test_utils.py
+++ pp/server/tests/test_utils.py
@@ -11,13 +11,3 @@ def test_which():
     assert util.which('vim') 
     assert not util.which('unknown')
 
-def test_checkEnvironmentNonExisting():
-    assert not util.checkEnvironment('FOO') 
-
-def test_checkEnvironmentNonExistingDirectory():
-    os.environ['FOO'] = '/foo'
-    assert not util.checkEnvironment('FOO') 
-
-def test_checkEnvironmentExistingDirectory():
-    os.environ['FOO'] = os.getcwd()
-    assert util.checkEnvironment('FOO') 
