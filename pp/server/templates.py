afbe42f3642c91ee516ff901023072f347f4e6c0
Andreas Jung <yet@gmx.de>
2024-10-17 18:48:15 +0200

    updated

--- pp/server/templates.py
+++ pp/server/templates.py
@@ -2,18 +2,23 @@
 # pp.server - Produce & Publish Server
 # (C) 2021, ZOPYX,  Tuebingen, Germany
 ################################################################
-""" Entry poing for generating circusd template ini """
+"""Entry poing for generating circusd template ini"""
 
 import os
 
-import pkg_resources
+import pkgutil
+
+
+def load_resource(package, resource_name):
+    data = pkgutil.get_data(package, resource_name)
+    return data
 
 
 def main():
     """Generate circusd.ini and server.ini"""
 
     for filename in ("circusd.ini", "server.ini"):
-        data = pkg_resources.resource_string("pp.server._templates", filename)
+        data = load_resource("pp.server._templates", filename)
         output_filename = os.path.join(os.path.abspath(os.getcwd()), filename)
         print("Generating ", output_filename)
 
